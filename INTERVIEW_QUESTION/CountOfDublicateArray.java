f20d7c103daa47578a93f0d7be58892dc186a4ea
Manojkumar Murugesan <101208123+Manojkumar4636@users.noreply.github.com>
2022-05-21 14:38:05 +0530

    Initial Commit

--- INTERVIEW_QUESTION/CountOfDublicateArray.java
+++ INTERVIEW_QUESTION/CountOfDublicateArray.java
@@ -61,12 +61,45 @@ public class CountOfDublicateArray {
 		System.out.println(duplicate);
 	}
 	
+	
+	//How many Count the Unique value in the given array
+	public void method3() {
+		
+		int[] given = {10,20,30,10,20,30,10,20,30,40};
+		int[] freq = new int[given.length];
+		
+		for(int i=0;i<given.length;i++) {
+			int no = given[i];
+			
+			int count = 1;
+			for(int j=i+1;j<given.length;j++) {
+				if(no==given[j]) {
+					count++;
+					freq[j] = -1;
+				}
+			}
+			if(freq[i]!=-1) {
+				freq[i] = count;
+			}
+		}
+		int Unique = 0;
+		for(int i=0;i<freq.length;i++) {
+			if(freq[i]==1) {
+				uniqe++;
+			}
+		}
+		System.out.println(Unique);
+		
+
+	}
+	
 
 
 	public static void main(String[] args) {
 		CountOfDublicateArray call = new CountOfDublicateArray();
 		call.method1();
 		call.method2();
+		call.method3();
 	}
 
 }
